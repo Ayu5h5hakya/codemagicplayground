b1badbf9eb1bf3ab372c39ee50293c8b37447ec1
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2021-12-08 10:25:15 +0545

    add body for widget testing logic

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,13 @@
+import 'package:codemagicpractice/my_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('This widget has a title and a descriptin', (tester) async {});
+  testWidgets('This widget has a title and a descriptin', (tester) async {
+    await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
+
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
+  });
 }
